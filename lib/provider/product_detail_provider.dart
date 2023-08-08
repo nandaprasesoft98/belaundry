853d832075bab80f63fc54c5d2cf8806a565b900
Nanda Prasetio <nandaprasesoft98@gmail.com>
2023-08-08 10:59:46 +0700

    product detail and list

--- lib/provider/product_detail_provider.dart
+++ lib/provider/product_detail_provider.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
-class ProductDetailProvider extends ChangeNotifier {
+import '../domain/entity/product/product.dart';
+import '../domain/repository/product_repository.dart';
+import '../misc/load_data_result.dart';
 
+import '../misc/httpclient/httpclientfutureprocessingcancellationcreator/http_client_future_processing_cancellation_creator.dart';
+import '../misc/load_data_result.dart';
+import '../misc/manager/api_request_manager.dart';
+
+class ProductDetailProvider extends ChangeNotifier {
+  final HttpClientFutureProcessingCancellationCreator cancellationCreator;
+  final ApiRequestManager apiRequestManager;
+  final ProductRepository productRepository;
+
+  ProductDetailProvider({
+    required this.cancellationCreator,
+    required this.apiRequestManager,
+    required this.productRepository
+  });
+
+  LoadDataResult<Product> _productDetailListLoadDataResult = NoLoadDataResult<Product>();
+  LoadDataResult<Product> get productDetailListLoadDataResult => _productDetailListLoadDataResult;
+  set productDetailListLoadDataResult(LoadDataResult<Product> value) {
+    _productDetailListLoadDataResult = value;
+    notifyListeners();
+  }
 }
--- lib/provider/product_list_provider.dart
+++ lib/provider/product_list_provider.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
-class ProductListProvider extends ChangeNotifier {
+import '../domain/repository/product_repository.dart';
+import '../misc/httpclient/httpclientfutureprocessingcancellationcreator/http_client_future_processing_cancellation_creator.dart';
+import '../misc/manager/api_request_manager.dart';
 
+class ProductListProvider extends ChangeNotifier {
+  final HttpClientFutureProcessingCancellationCreator cancellationCreator;
+  final ApiRequestManager apiRequestManager;
+  final ProductRepository productRepository;
+
+  ProductListProvider({
+    required this.cancellationCreator,
+    required this.apiRequestManager,
+    required this.productRepository
+  });
 }
